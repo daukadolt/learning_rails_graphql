26cbe3656b3d95a3ccce18033975573950534a53
dauletamirkhanov <damirkhanov01@gmail.com>
2021-06-08 16:22:55 +0600

    author and book types

---
+++ app/graphql/types/book_type.rb
@@ -0,0 +1,7 @@
+module Types
+  class BookType < Types::BaseObject
+    field :id, ID, null: false
+    field :title, String, null: false
+    field :author, Types::AuthorType, null: false
+  end
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -7,11 +7,20 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :books, [Types::BookType], null: true do
+      description "All the books"
+    end
+
+    field :authors, [Types::AuthorType], null: true do
+      description "All the authors"
+    end
+
+    def books
+      []
+    end
+
+    def authors
+      []
     end
   end
 end
